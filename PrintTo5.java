5b4aa680ed86769fe063f9c58b7f1c20fa32f2bb
alonnitzan <alon.nit@gmail.com>
2024-11-04 17:47:45 +0200

    try to save

--- PrintTo5.java
+++ PrintTo5.java
@@ -3,7 +3,6 @@
 public class PrintTo5 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
-        int x = 0;
         int i = 0;
         // Complete the program's code here
     }
